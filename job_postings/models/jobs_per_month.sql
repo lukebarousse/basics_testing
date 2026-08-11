39bb500bb6d15f8c3b2949e54c04ef827418d7cb
Luke Barousse <luke.barousse@gmail.com>
2026-08-11 16:04:22 -0500

    Project #1

---
+++ job_postings/models/jobs_per_month.sql
@@ -0,0 +1,10 @@
+{# {{ config(materialized="ephemeral") }} #}
+
+-- monthly posting counts by role
+SELECT
+    DATE_TRUNC('month', search_date) AS month,
+    search_term AS role,
+    COUNT(*) AS postings
+FROM {{ ref('job_postings') }}
+GROUP BY ALL
+ORDER BY month, role
